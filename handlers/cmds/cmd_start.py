1b6c82abd33614b71307c834abe881089c61d6cb
Fedor <feda.vert@gmail.com>
2026-01-30 12:54:49 +0300

    Small changes in 'cmd_start.py'

--- handlers/cmds/cmd_start.py
+++ handlers/cmds/cmd_start.py
@@ -7,7 +7,10 @@ router = Router()
 
 @router.message(CommandStart())
 async def process_cmd_start(message: Message):
-    text = f"👋 Привет, {message.from_user.username}!"
+    text = (f"👋 Привет, {message.from_user.username}!\n"
+            "Я - бот, у которого ты можешь купить различные товары.\n"
+            "Используй команды в меню, чтобы узнать больше!")
+
 
     await message.answer(
         text=text
